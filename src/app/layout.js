73dcc362883aaf420fd69dd0f4c69a602d4b912e
bcat95 <bcat9505@gmail.com>
2024-07-24 10:06:30 +0700

    Adsence

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,6 +12,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="vi">
+            <Head>
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                <script>
+                    {`(adsbygoogle = window.adsbygoogle || []).push({
+                        google_ad_client: "ca-pub-3682138305573283",
+                        enable_page_level_ads: true
+                    });`}
+                </script>
+            </Head>
             <body className={inter.className}>{children}</body>
         </html>
     )
--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-6">
             <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
-                <h1 className="text-4xl font-bold mb-8 text-center"><a href="https://zalo.me/g/myewnj459">RioMCN</a> Văn bản thành Lời nói</h1>
+                <h1 className="text-4xl font-bold mb-8 text-center"><a href="https://zalo.me/g/myewnj459">Chatvn</a> Văn bản thành Lời nói</h1>
                 <Suspense fallback={<p>Loading form...</p>}>
                     <TextToSpeechForm />
                 </Suspense>
